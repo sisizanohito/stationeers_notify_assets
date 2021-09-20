ef686ffcc0fb2a22f57d96be83e8b284b3f60acd
sisizanohito <sisizanohito@gmail.com>
2021-09-20 15:42:59 +0900

    スクリプトの更新

--- bake_and_notify.sh
+++ bake_and_notify.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
+cd `dirname $0`
 
+discord_token={{DISCORD_TOKEN}}
 
+cd stationeers_resources/locales
+
+git fetch
+
+master_commit_id=`git show -s master --format=%H`
+origin_commit_id=`git show -s origin/master --format=%H`
+
+echo "master_commit_id:${master_commit_id}"
+echo "origin_commit_id:${origin_commit_id}"
+
+if [[ "${master_commit_id}" == "${origin_commit_id}" ]]; then
+  echo "same commit"
+else
+  echo "new commit"
+
+  echo "clear and update"
+  cd ../
+  git fetch
+  git reset --hard origin/master
+  git clean -d -f
+  git submodule update --remote
+  echo "done(clear and update)"
+
+  echo "bake and notify"
+  martian bake
+  sh release.sh
+  martian notify --token ${discord_token} assets
+  echo "done(bake and notify)"
+fi
